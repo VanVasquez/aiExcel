a39c7a7cd84f9e643af8f3437e4dafbd9388376b
Van Vincent Vasquez <113902421+VanVasquez@users.noreply.github.com>
2023-07-23 13:15:50 +0800

    All About Tab edited

--- src/components/Textblock/index.jsx
+++ src/components/Textblock/index.jsx
@@ -118,7 +118,43 @@ const TextBlock = () => {
                 </p>
                 <h2>Team Information</h2>
                 <p>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AiExcel was founded by Computer Science students of ICCT Colleges, an AI enthusiastic team with a strong commitment to revolutionizing the way students interact with artificial intelligence. At AiExcel, our team is composed of passionate individuals from diverse backgrounds, all driven by a common goal of supporting students and advancing the accessibility of AI technologies in education. Our team members bring together a unique blend of expertise in collaboration, web development, education, and user experience design. We work collaboratively to ensure that AiExcel provides the best possible resources and user experience for our valued visitors.
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AiExcel was founded by Computer Science students of ICCT Colleges, an AI enthusiastic team with a strong commitment to revolutionizing the way students interact with artificial intelligence. At AiExcel, our team is composed of passionate individuals from diverse backgrounds, all driven by a common goal of supporting students and advancing the accessibility of AI technologies in education. 
+                  <br></br>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Our team members bring together a unique blend of expertise in collaboration, web development, education, and user experience design. We work collaboratively to ensure that AiExcel provides the best possible resources and user experience for our valued visitors.
+                </p>
+                <h2>Privacy Policy</h2>
+                <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; At AiExcel, we take the privacy and security of our users' information seriously. This Privacy Policy outlines how we collect, use, and protect personal data when you use our web platform. By accessing and using AiExcel, you agree to the terms described in this policy.
+                </p>
+                <br />
+                <p>
+                  <h3>Information Collection:</h3>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We may collect certain personally identifiable information from users, such as names, email addresses, and educational backgrounds, when they register on our platform or voluntarily provide it while using our services.
+                </p>
+                <br />
+                <p>
+                  <h3>Usage of Information:</h3>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The information we collect is used to improve our services, personalize user experience, and communicate important updates. We may also use aggregated and anonymized data for analytical purposes to enhance the functionality and content of AiExcel.
+                </p>
+                <p>
+                  <h3>Data Security:</h3>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We implement industry-standard security measures to protect user data from unauthorized access, alteration, disclosure, or destruction. However, please note that no method of data transmission over the internet or electronic storage is 100% secure, therefore, we will try to guarantee absolute security.
+                </p>
+                <br></br>
+                <p>
+                  <h3>Third-Party Links:</h3>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AiExcel may contain links to third-party websites, tools, or resources. Please be aware that we are not responsible for the privacy practices or content of these external sites. We encourage users to read the privacy policies of any linked websites before providing any personal information.
+                </p>
+                <h2>Contact Information</h2>
+                <p>
+                For any inquiries, feedback, or support related to AiExcel, you can reach us using the following contact information:
+                </p>
+                <h3>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Email: aiexcel.customercs@gmail.com <br></br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; C-Phone: 09916980658
+                </h3>
+                <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Our team is dedicated to assisting you with any questions you may have about our web platform, the AI-powered tools and resources we offer, or any other related matters. Whether you are a student seeking assistance or a user with suggestions for improvement, we value your input and look forward to hearing from you.
                 </p>
               </div>
             </Tab>{" "}
